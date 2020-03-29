ae24dfdf2ae85ceed8a8b37459469f63491e8bee
megj4917 <59276924+megj4917@users.noreply.github.com>
2020-03-28 21:54:02 -0700

    Update send.php

--- send.php
+++ send.php
@@ -1,19 +1,14 @@
 <?php
-
-    # This code was written for the contact form
-    # Github only supports static sights so this can not be used
-    # For future reference, if another hosting platform is used this can be used for the contact form
-
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
-	$email_subject = $-POST['subject'];
+	$email_subject = $_POST['subject'];
 
 	$email_body = "Name: $name.\n".
 				  "Email: $email.\n".
 				  "Message: $message.\n";
 
-	$email_to = 'meganjacob2004@yahoo.com';
+	$email_to = 'meganjacob2004@gmail.com';
 
 	mail($email_to, $email_subject, $email_body);
 	header("Location: contact.html");
